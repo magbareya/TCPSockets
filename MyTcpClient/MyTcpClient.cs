b85ec20ac9046b6cbb553b6edf4637769c1eb0d6
magbareya <mahmoud.agbareya@gmail.com>
2013-11-10 21:21:02 +0200

    Add the porject

---
+++ MyTcpClient/MyTcpClient.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace MyTcpClient
+{
+    class MyTcpClient
+    {
+        static void Main(string[] args)
+        {
+            var client = new TcpClient();
+            client.Connect(new IPEndPoint(IPAddress.Parse("16.54.177.43"), 8091));
+            Console.WriteLine("Client started!");
+            var stream = client.GetStream();
+            var request = "Hello Worl! This is my first TCP socket program. Thanks you!!";
+            var requestBuff = Encoding.UTF8.GetBytes(request);
+            stream.Write(requestBuff, 0, requestBuff.Length);
+            stream.Flush();
+            var response = new byte[1024];
+            var length = stream.Read(response, 0, response.Length);
+            Console.WriteLine("Response was received: {0}", Encoding.UTF8.GetString(response, 0, length));
+            client.Close();
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpServer/TcpServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TcpServer
+{
+    class TcpServer
+    {
+        static void Main(string[] args)
+        {
+            var port = 8091;
+            TcpListener listener = new TcpListener(IPAddress.Parse("16.54.177.43"), port);
+            listener.Start();
+            Console.WriteLine("TCP Listener started on port {0}", port);
+            var buff = new byte[256];
+            try
+            {
+                var client = listener.AcceptTcpClient();
+                Console.WriteLine("Connected!!");
+                var stream = client.GetStream();
+                int i = 0;
+                Console.WriteLine("Received data:");
+                while ((i = stream.Read(buff, 0, buff.Length)) != 0)
+                {
+                    var data = Encoding.UTF8.GetString(buff, 0, i);
+                    Console.Write(data);
+                    i = 0;
+                    var response = Encoding.UTF8.GetBytes("Thank you!!");
+                    stream.Write(response, 0, response.Length);
+                }
+                client.Close();
+            }
+            finally
+            {
+                listener.Stop();
+            }
+            Console.WriteLine("DONE");
+            Console.ReadLine();
+
+        }
+    }
+}
